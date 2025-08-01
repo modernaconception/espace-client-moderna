c5164aac9143314a81525e41a1633662cb4565d8
modernaconception <lucagiarratano.pro@gmail.com>
2025-08-01 08:38:55 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Bienvenue sur votre Espace Client</h1>
+      <p>Ceci est l’interface de suivi de vos chantiers.</p>
+    </div>
+  );
+}
+
+export default App;
